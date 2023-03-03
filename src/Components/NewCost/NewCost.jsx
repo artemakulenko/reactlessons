3d70befc11203ef925bbe48cca35f97b631d62d4
artemakulenko <akulenkoar@yandex.ru>
2023-03-04 01:42:15 +0300

    initial commit

--- src/Components/NewCost/NewCost.jsx
+++ src/Components/NewCost/NewCost.jsx
@@ -1,4 +1,4 @@
-import styles from './NewCost.module.scss'
+// import styles from './NewCost.module.scss'
 import { Container } from '../UI/Container/Container'
 import { Card } from '../UI/Card'
 import { CostForm } from '../CostForm'
