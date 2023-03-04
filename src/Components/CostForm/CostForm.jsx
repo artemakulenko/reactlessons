c333c9c9123e8db89836a1e0dd99c64824164ad0
artemakulenko <akulenkoar@yandex.ru>
2023-03-04 20:40:51 +0300

    temp

--- src/Components/CostForm/CostForm.jsx
+++ src/Components/CostForm/CostForm.jsx
@@ -1,13 +1,30 @@
+import { useState } from 'react'
 import styles from './CostForm.module.scss'
 
 const CostForm = (props) => {
 
+  const [userInput, setUserInput] = useState({
+    name: '',
+    amount: '',
+    date: ''
+  })
+
+  const nameChangeHandler = (e) => setUserInput({ ...userInput, name: e.target.value})
+  const amountChangeHandler = (e) => setUserInput({...userInput, amount: e.target.value})
+  const dateChangeHandler = (e) => setUserInput({...userInput, date: e.target.value})
+
+
+
+
   return(
     <>
+      <div> name: {userInput.name} </div>
+      <div> amount: {userInput.amount} </div>
+      <div> date: {userInput.date} </div>
       <form className={styles.form}  >
         <div className={styles.formGroup}>
           <label htmlFor="text">Название</label>
-          <input type="text" placeholder='Название'/>
+          <input type="text" placeholder='Название' onChange={nameChangeHandler}/>
         </div>
 
         <div className={styles.formGroup}>
@@ -16,12 +33,14 @@ const CostForm = (props) => {
             type="number" 
             placeholder='Сумма' 
             min={0.01} 
-            step={0.01} />
+            step={0.01} 
+            onChange={amountChangeHandler}
+            />
         </div>
 
         <div className={styles.formGroup}>
           <label htmlFor="">Сумма</label>
-          <input type="date" placeholder='' min='2019-01-01' max='2022-12-31'/>
+          <input type="date" placeholder='' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler}/>
         </div>
 
         <div className={styles.buttonWrapper}>
